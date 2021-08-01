6227f2289cca16c3aea8256c4062405a4cf93229
Disco Adina <dadina@si-n4.kzn.21-school.ru>
2021-08-01 21:24:22 +0300

    second commit

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ function draw() {
         x: 500,
         y: 500,
         radius: 50,
-        color: '#0095DF',
+        color: 'blue',
         draw: function () {
             ctx.beginPath();
             ctx.arc(ball.x , ball.y , this.radius, 0, Math.PI * 2, true);
@@ -18,66 +18,28 @@ function draw() {
     canvas.addEventListener('mousemove', function(e) {
         if (!running) {
             clear();
-            ball.x = e.clientX;
-            ball.y = e.clientY;
-            ctx.beginPath();
-            ctx.arc(600, 600, 395, 0, Math.PI * 2, true);
-            ctx.clip();
-
-            while (ball.y > 945)
-                ball.y--;
-            while (ball.y < 255)
-                ball.y++;
-            while (ball.x > 945)
-                ball.x--;
-            while (ball.x < 255)
-                ball.x++;
+			let x = e.clientX
+			let y = e.clientY
+			let c = Math.sqrt(((x - 600) ** 2) + ((y - 600) ** 2))
+			if (c < 346) {
+				ball.x = e.clientX;
+				ball.y = e.clientY;
+			}
+			else{
+				let k = 350 / c;
+				ball.x = 600 + (x - 600) * k;
+				ball.y = 600 + (y - 600) * k;
+			}
             ball.draw();
         }
     });
     function clear() {
-        // ctx.fillStyle = "white";
-        // ctx.arc(600, 600, 390, 0, 2 * Math.PI);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        ctx.fillStyle = '#0061a1';
+        ctx.fillStyle = 'blue';
         ctx.arc(600, 600, 400, 0, 2 * Math.PI);
         ctx.arc(600, 600, 395, 0, 2 * Math.PI);
         ctx.fill('evenodd');
         ctx.closePath();
-        // ctx.fill();
     }
-    // ctx.beginPath();
-    // ctx.fillStyle = '#0061a1';
-    // ctx.arc(600, 600, 400, 0, 2 * Math.PI);
-    // ctx.arc(600, 600, 395, 0, 2 * Math.PI);
-    // ctx.fill('evenodd');
-    // ctx.closePath();
-    // ctx.fill();
-    function draw() {
-        clear();
-        // ball.draw();
-        // ball.x += ball.vx;
-        // ball.y += ball.vy;
-        // if (ball.y + ball.vy > 400 || ball.y + ball.vy -50 < 0) {
-        //     ball.vy = -ball.vy;
-        // }
-        // if (ball.x + ball.vx > 400 || ball.x + ball.vx - 50 < 0) {
-        //     ball.vx = -ball.vx;
-        // }
-        ball.draw();
-    }
-    clear();
-    draw();
-// console.log(123);
-    // while (1) {
-   //     ctx.beginPath();
-   //      ctx.fillStyle = '#0095DF';
-   //      ctx.arc(10 + m.x, 10 + m.y , 55, 0, 2 * Math.PI);
-   //      ctx.fill();
-   //  // }
-   //  ctx.closePath();
-    // ball.draw()
-    // ctx.arc(60, 60, 40, 0, 2 * Math.PI);
-    // ctx.fill()
 }
